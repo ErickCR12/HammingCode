d8123f1f79b11321d8a1113f54e20e332bf3caac
Dennis Jimenez <alejimenezc@hotmail.com>
2023-03-27 18:36:34 -0600

    Corregida decodificacion y agregada la matriz para codificacion

--- Hamming.py
+++ Hamming.py
@@ -1,88 +1,93 @@
-#Calcule el codigo hamming (15,11) para cualquier mensaje de 11 bits
-#Autor: Jose Luis Macias Urbina
-#Fecha: 10/10/2016
-#Version: 1.0
-#Plataforma: Python v2.7
-#Descripcion: El codigo hamming es un codigo de paridad que permite detectar y corregir errores en un mensaje de 11 bits
-#Ejemplo: 10101010101
-
-#Importamos la libreria sys para poder usar la funcion exit
-import sys
-
-#Definimos la funcion para calcular el codigo hamming
+# Definimos la funcion para calcular el codigo hamming
 def hamming(mensaje):
-    #Creamos una lista para guardar el mensaje
+    # Creamos una lista para guardar el mensaje
     lista = []
+    listaF = []
 
-
-    #Recorremos el mensaje y lo guardamos en la lista
+    # Recorremos el mensaje y lo guardamos en la lista
     for i in mensaje:
         lista.append(i)
 
-    #Calculamos los bits de paridad
-    p1 = int(lista[0]) ^ int(lista[1]) ^ int(lista[3]) ^ int(lista[4]) ^ int(lista[6]) ^ int(lista[8]) ^ int(lista[10])  # Correcto
-    p2 = int(lista[0]) ^ int(lista[2]) ^ int(lista[3]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[9]) ^ int(lista[10])  # Correcto
-    p4 = int(lista[1]) ^ int(lista[2]) ^ int(lista[3]) ^ int(lista[7]) ^ int(lista[8]) ^ int(lista[9]) ^ int(lista[10])  # Correcto
-    p8 = int(lista[4]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[7]) ^ int(lista[8]) ^ int(lista[9]) ^ int(lista[10])  # Correcto
+    # Calculamos los bits de paridad
+    p1 = int(lista[0]) ^ int(lista[1]) ^ int(lista[3]) ^ int(lista[4]) ^ int(lista[6]) ^ int(lista[8]) ^ int(
+        lista[10])  # Correcto
+    p2 = int(lista[0]) ^ int(lista[2]) ^ int(lista[3]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[9]) ^ int(
+        lista[10])  # Correcto
+    p4 = int(lista[1]) ^ int(lista[2]) ^ int(lista[3]) ^ int(lista[7]) ^ int(lista[8]) ^ int(lista[9]) ^ int(
+        lista[10])  # Correcto
+    p8 = int(lista[4]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[7]) ^ int(lista[8]) ^ int(lista[9]) ^ int(
+        lista[10])  # Correcto
 
-    #Imprimimos el mensaje original
-    print ("Mensaje original: " + mensaje)
+    # Imprimimos el mensaje original
+    print("Mensaje original: " + mensaje)
 
-    #Imprimimos el mensaje con los bits de paridad
-    print ("Mensaje con bits de paridad: " + str(p1) + str(p2) + lista[0] + str(p4) + lista[1] + lista[2] + lista[3] + str(p8) + lista[4] + lista[5] + lista[6] + lista[7] + lista[8] + lista[9] + lista[10])
+    # Imprimimos el mensaje con los bits de paridad
+    print(
+        "Mensaje con bits de paridad: " + str(p1) + str(p2) + lista[0] + str(p4) + lista[1] + lista[2] + lista[3] + str(
+            p8) + lista[4] + lista[5] + lista[6] + lista[7] + lista[8] + lista[9] + lista[10])
 
-    #Calculamos el codigo hamming
-    codigo = str(p1) + str(p2) + lista[0] + str(p4) + lista[1] + lista[2] + lista[3] + str(p8) + lista[4] + lista[5] + lista[6] + lista[7] + lista[8] + lista[9] + lista[10]
+    # Calculamos el codigo hamming
+    codigo = str(p1) + str(p2) + lista[0] + str(p4) + lista[1] + lista[2] + lista[3] + str(p8) + lista[4] + lista[5] + \
+             lista[6] + lista[7] + lista[8] + lista[9] + lista[10]
 
-    #Imprimimos el codigo hamming
+    # Imprimimos el codigo hamming
     print("Codigo hamming: " + codigo)
 
-    #Retornamos el codigo hamming
-    return codigo
+    for i in codigo:
+        listaF.append(i)
 
-#Definimos la funcion para calcular el mensaje original
+    MH = [[" ", " ", lista[0], " ", lista[1], lista[2], lista[3], " ", lista[4], lista[5], lista[6], lista[7], lista[8],
+           lista[9], lista[10]],
+          [str(p1), 0, lista[0], 0, lista[1], 0, lista[3], 0, lista[4], 0, lista[6], 0, lista[8], 0, lista[10]],
+          [0, str(p2), lista[0], 0, 0, lista[2], lista[3], 0, 0, lista[5], lista[6], 0, 0, lista[9], lista[10]],
+          [0, 0, 0, str(p4), lista[1], lista[2], lista[3], 0, 0, 0, 0, lista[7], lista[8], lista[9], lista[10]],
+          [0, 0, 0, 0, 0, 0, 0, str(p8), lista[4], lista[5], lista[6], lista[7], lista[8], lista[9], lista[10]],
+          [listaF[0], listaF[1], listaF[2], listaF[3], listaF[4], listaF[5], listaF[6], listaF[7], listaF[8], listaF[9], listaF[10], listaF[11], listaF[12], listaF[13], listaF[14]]]
+
+    print(MH)
+
+    # Retornamos el codigo hamming
+    return MH
+
+
+
+
+# Genere una funcion que permita calcular el mensaje original a partir del codigo hamming
 def mensaje(codigo):
-    #Creamos una lista para guardar el codigo
+    # Creamos una lista para guardar el codigo
     lista = []
 
-    #Recorremos el codigo y lo guardamos en la lista
+    # Recorremos el codigo y lo guardamos en la lista
     for i in codigo:
         lista.append(i)
 
-    #Calculamos los bits de paridad
-    p1 = int(lista[0]) ^ int(lista[1]) ^ int(lista[3]) ^ int(lista[4]) ^ int(lista[6]) ^ int(lista[8]) ^ int(lista[10]) #Correcto
-    p2 = int(lista[0]) ^ int(lista[2]) ^ int(lista[3]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[9]) ^ int(lista[10]) #Correcto
-    p4 = int(lista[1]) ^ int(lista[2]) ^ int(lista[3]) ^ int(lista[7]) ^ int(lista[8]) ^ int(lista[9]) ^ int(lista[10]) #Correcto
-    p8 = int(lista[4]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[7]) ^ int(lista[8]) ^ int(lista[9]) ^ int(lista[10]) #Correcto
+    # Calculamos los bits de paridad
+    p1 = int(lista[0]) ^ int(lista[2]) ^ int(lista[4]) ^ int(lista[6]) ^ int(lista[8]) ^ int(lista[10]) ^ int(
+        lista[12]) ^ int(lista[14])  # Correcto
+    p2 = int(lista[1]) ^ int(lista[2]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[9]) ^ int(lista[10]) ^ int(
+        lista[13]) ^ int(lista[14])  # Correcto
+    p4 = int(lista[3]) ^ int(lista[4]) ^ int(lista[5]) ^ int(lista[6]) ^ int(lista[11]) ^ int(lista[12]) ^ int(
+        lista[13]) ^ int(lista[14])  # Correcto
+    p8 = int(lista[7]) ^ int(lista[8]) ^ int(lista[9]) ^ int(lista[10]) ^ int(lista[11]) ^ int(lista[12]) ^ int(
+        lista[13]) ^ int(lista[14])  # Correcto
 
-    #Calculamos el bit de error
-    error = p1 + p2 + p4 + p8
+    # Calculamos el error
+    error = str(p1) + str(p2) + str(p4) + str(p8)
 
-    #Imprimimos el codigo hamming
-    print("Codigo hamming: " + codigo)
+    # Imprimimos el mensaje original
+    print(
+        "Mensaje original: " + lista[2] + lista[4] + lista[5] + lista[6] + lista[8] + lista[9] + lista[10] + lista[11] +
+        lista[12] + lista[13] + lista[14])
 
-    #Imprimimos el bit de error
-    print("Bit de error: " + str(error))
+    # Imprimimos el error
+    print("Error: " + error)
 
-    #Si el bit de error es 0, no hay errores
-    if error == 0:
-        print("No hay errores")
-    #Si el bit de error es mayor a 0, hay errores
-    else:
-        print("Hay errores")
+    # Retornamos el mensaje original
+    return lista[2] + lista[4] + lista[5] + lista[6] + lista[8] + lista[9] + lista[10] + lista[11] + lista[12] + lista[
+        13] + lista[14]
 
-        #Si el bit de error es mayor a 15, hay mas de un error
-        if error > 15:
-            print("Hay mas de un error")
-        #Si el bit de error es menor a 15, hay un error
-        else:
-            print("Hay un error")
-
-            #Imprimimos el mensaje original
-            print("Mensaje original: " + lista[0] + lista[1] + lista[2] + lista[3] + lista[4] + lista[5] + lista[6] + lista[7] + lista[8] + lista[9] + lista[10])
-
-    #Retornamos el mensaje original
-    return
 
 hamming("11001001010")
-mensaje("101110011001010")
+#hamming("10101010101")
+#mensaje("101110011001010")
+#mensaje("101101001010101")
